7123e66ff9e1a8403f0ab08fa146e4b7364a86f5
Migue Magic <miguelitoeremenko@gmail.com>
2026-06-19 17:40:12 -0400

    fix: change vimtex viewer to zathura

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -1,9 +1,10 @@
 return {
   "lervag/vimtex",
-  lazy = false, -- Needed to be loaded early for proper LaTeX workflow
+   lazy = false, -- Needed to be loaded early for proper LaTeX workflow
    ft = "tex", -- Ensure it only loads for tex files
    config = function()
-     vim.g.vimtex_view_method = 'okular'
+     vim.g.vimtex_view_method = 'zathura'
+     vim.g.vimtex_compiler_method = 'latexmk'
   end
 }
 
