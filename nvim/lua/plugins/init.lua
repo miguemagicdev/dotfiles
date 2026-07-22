9e8b1290e8368eb792133b4123175bfd8a8fff6d
Migue Magic <miguelitoeremenko@gmail.com>
2026-07-21 23:45:03 -0400

    fix: delete inexistent require in `init.lua`

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,7 +5,6 @@ local plugins = {
   require("plugins.treesitter"),
   require("plugins.lsp"),
   require("plugins.cmp"),
-  require("plugins.nvim-cmp"),
   require("plugins.luasnip"),
   require("plugins.autopairs"),
   require("plugins.vimtex"),
