44de3b742780e39a1bd2b0f92a1de855ae7dde6b
Migue Magic <miguelitoeremenko@gmail.com>
2026-08-27 22:54:33 -0400

    feat: add fzf zsh completion

--- dot_zshrc.d/04-completion.zsh
+++ dot_zshrc.d/04-completion.zsh
@@ -17,3 +17,6 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
 
 # Make Tab accept the suggestion
 bindkey '^I' autosuggest-accept
+
+# Set up fzf key bindings and fuzzy completions
+source <(fzf --zsh)
